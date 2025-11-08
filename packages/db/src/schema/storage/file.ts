05bbd461a26db483a5e10326e06b6e2904e94b85
ahargunyllib <billy.bpm03@gmail.com>
2025-11-08 18:13:55 +0700

    feat: Add schema definitions for file and folder tables

---
+++ packages/db/src/schema/storage/file.ts
@@ -0,0 +1,25 @@
+import { integer, pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { user } from "../auth";
+import { folder } from "./folder";
+
+export const file = pgTable("file", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  originalName: text("original_name").notNull(),
+  mimeType: text("mime_type").notNull(),
+  size: integer("size").notNull(),
+  path: text("path").notNull(),
+  url: text("url").notNull(),
+  folderId: text("folder_id")
+    .references(() => folder.id, {
+      onDelete: "cascade",
+    })
+    .notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  category: text("category").notNull(),
+  deletedAt: timestamp("deleted_at"),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
---
+++ packages/db/src/schema/storage/folder.ts
@@ -0,0 +1,12 @@
+import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { user } from "../auth";
+
+export const folder = pgTable("folder", {
+  id: text("id").primaryKey(),
+  name: text("name").notNull(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => user.id, { onDelete: "cascade" }),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
